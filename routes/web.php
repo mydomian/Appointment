96d0d1293d90f674f468ff749ba2e65210d690f2
mydomian <mortuza504@gmail.com>
2022-05-12 18:07:49 +0600

    shakilupdate first commit

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return view('layouts.enterprener.home');
+
+// });
+
+Route::get('/','IndexController@home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('mentor/list','EnterprenerController@index')->name('mentor.list');
+Route::get('mentor/{id}','EnterprenerController@showDoctor');
+
+// Enterprener
+Route::prefix('/user')->group(function(){
+    Route::group(['middleware'=>['auth']],function(){
+        Route::post('appointment','AppointmentController@store')->name('appointment');
+        Route::get('profile/{id}','EnterprenerController@profile')->name('user.profile');
+        Route::post('payment','EnterprenerController@Payment');
+    });
+});
+
+
 
 //admin
 Route::prefix('/admin')->namespace('Admin')->group(function(){
@@ -37,26 +55,11 @@ Route::prefix('/admin')->namespace('Admin')->group(function(){
 Route::prefix('/mentor')->namespace('Mentor')->group(function(){
 
     Route::match(['get','post'],'/','MentorController@Login');
-    Route::get('registration','MentorController@Registration');
-    Route::post('registration-store','MentorController@RegistrationStore');
-
     Route::group(['middleware'=>['mentor']],function(){
         Route::get('dashboard','MentorController@Dashboard');
-        Route::get('appointment-lists','MentorController@AppointLists');
-        Route::get('appointment-info/{id}','MentorController@AppointInfo');
-        Route::get('appointment-pending/{id}','MentorController@AppointmentPending');
-        Route::get('appointment-reject/{id}','MentorController@AppointmentReject');
-        Route::get('appointment-accept/{id}','MentorController@AppointmentAccepted');
-        Route::get('appointment-communication/{id}','MentorController@CommunicationId');
-        Route::post('communication-store','MentorController@CommunicationStore');
-        Route::get('payment-accept/{id}','MentorController@PaymentAccept');
-        Route::get('payment-reject/{id}','MentorController@PaymentReject');
-        Route::get('payments-info','MentorController@PaymentsInfo');
-        Route::get('password-change','MentorController@PasswordChange');
-        Route::post('password-store','MentorController@PasswordStore');
-        Route::get('logout','MentorController@Logout');
     });
 
 });
+
 
 
