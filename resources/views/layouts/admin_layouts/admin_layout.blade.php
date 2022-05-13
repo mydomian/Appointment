436a5bf883fd49ff8d5c034e7f0ce7f84686a5f2
Shakil Hussain <shakil550sh@gmail.com>
2022-05-13 15:30:15 +0600

    admin title change

--- resources/views/layouts/admin_layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layouts/admin_layout.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>Admin | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -27,9 +27,9 @@
   <link rel="stylesheet" href="{{ asset('plugins/jqvmap/jqvmap.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
-  
 
-  
+
+
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Daterange picker -->
@@ -74,9 +74,9 @@
     <!-- Main content -->
     <section class="content">
       @yield('mainContent')
-      
-      
-      
+
+
+
 
     </section>
     <!-- /.content -->
@@ -165,7 +165,7 @@
       "responsive": true, "lengthChange": true, "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     });
-  
+
   });
 </script>
 
--- resources/views/layouts/admin_layouts/sidebar.blade.php
+++ resources/views/layouts/admin_layouts/sidebar.blade.php
@@ -12,7 +12,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Admin</a>
         </div>
       </div>
 
@@ -68,13 +68,13 @@
               </li>
             </ul>
           </li>
-          
-          
-        
-          
-              
-             
-        
+
+
+
+
+
+
+
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
